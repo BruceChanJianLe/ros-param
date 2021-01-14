a3384fc4009f183ae9d99ca17a206f69f3b3144f
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-01-14 08:38:54 +0800

    update source file

---
+++ src/ros_param.cpp
@@ -0,0 +1,20 @@
+#include "ros-param/ros_param.hpp"
+
+
+namespace ros_param
+{
+    loader::loader()
+    {
+    }
+
+
+    loader::~loader()
+    {
+    }
+
+
+    void loader::run()
+    {
+        ;
+    }
+} // namespace ros_param
