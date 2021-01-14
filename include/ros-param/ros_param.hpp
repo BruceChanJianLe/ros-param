f50c7277d2fad4dcbcc9917aa4e3588dcab459c0
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-01-14 08:38:35 +0800

    update header file

--- include/ros-param/ros_param.hpp
+++ include/ros-param/ros_param.hpp
@@ -1,1 +1,29 @@
-#ifndef 
+#ifndef ros_param_H__
+#define ros_param_H__
+
+#include <ros/ros.h>
+
+// Tools for retrieving ROS Param (mostly for catching errors)
+#include <xmlrpcpp/XmlRpc.h>
+
+#include <string>
+
+
+namespace ros_param
+{
+    class loader
+    {
+    private:
+        ros::NodeHandle private_nh_;
+        ros::NodeHandle global_nh_;
+
+    public:
+        loader();
+        ~loader();
+
+        void run();
+    };
+    
+} // namespace ros_param
+
+#endif
